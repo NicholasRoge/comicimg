6b4fe91b2f331c56ab617d0dde244a94f242264a
Nicholas Roge <NicholasRoge@gmail.com>
2016-09-21 23:04:59 -0500

    damn ide's not doing python well.

--- shaders.py
+++ shaders.py
@@ -14,16 +14,16 @@ class ComicShader():
 		self.apply(image)
 
     def apply(self, image):
-		pixels = image.load()
+        pixels = image.load()
         mask = CircleMask()
 
         size = 10
-		for x in xrange(0, image.size[0], size):
-			for y in xrange(0, image.size[1], size):
-				mask.region.x = x;
-				mask.region.y = y;
-				mask.region.width = min(size, image.size[0] - region.x)
-				mask.region.height = min(size, image.size[1] - region.y)
+        for x in xrange(0, image.size[0], size):
+            for y in xrange(0, image.size[1], size):
+                mask.region.x = x;
+                mask.region.y = y;
+                mask.region.width = min(size, image.size[0] - region.x)
+                mask.region.height = min(size, image.size[1] - region.y)
 
                 color = color.average(image, region)
                 hls = rgb_to_hls(color)
